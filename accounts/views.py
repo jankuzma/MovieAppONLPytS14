5d2646369a4e14dea73ccaea857e7c7c1aae092c
cooklee <sbbogus@gmail.com>
2023-07-24 11:02:19 +0200

    rejestracja uytkownika

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
+from sqlite3 import IntegrityError
+
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.views import View
 
@@ -23,3 +26,26 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect('index')
+
+
+class RegisterView(View):
+
+    def get(self, request):
+        return render(request, 'accounts/register.html')
+
+    def post(self, request):
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 == password2:
+            u = User(first_name=first_name, last_name=last_name,username=username)
+            u.set_password(password1)
+            try:
+                u.save()
+            except Exception:
+                return render(request, 'accounts/register.html', {'msg': 'użytkownik o takiej nazwie już istnieje'})
+            login(request, u)
+            return redirect('index')
+        return render(request, 'accounts/register.html', {'msg':'hasła nie sa takie same'})
