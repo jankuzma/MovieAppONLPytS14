8943c1500183673778d510beff223cfbc81e6e59
cooklee <sbbogus@gmail.com>
2023-07-24 10:12:10 +0200

    movie

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'accounts/login.html')
+
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password) #autentykacja
+        if user is not None:
+            login(request, user) #autoryzacja
+            return redirect('index')
+        return render(request, 'accounts/login.html')
